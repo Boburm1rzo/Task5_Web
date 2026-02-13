4cd78ebde1d32f0a72d45a2b8cd69050b5c8c9ed
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-02-13 17:55:22 +0500

    added new link from prod

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { SongDetailsDto, SongsPageResponse } from "../types";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
 });
 
 export async function getSongs(params: {
